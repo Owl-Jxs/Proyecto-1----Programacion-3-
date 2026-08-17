8bda8001b294e05d27dea8dc8119c062af4af355
Daniel <alvasola15@gmail.com>
2026-08-16 21:06:17 -0600

    inicio proyecto

---
+++ src/AplicacionGestora/Main.java
@@ -0,0 +1,9 @@
+package AplicacionGestora;
+//import AplicacionGestora.Interfaz.*;
+//import AplicacionGestora.Logica.*;
+
+public class Main {
+    public static void main (String [] args) {
+        System.out.println( "Hello world");
+    }
+}
