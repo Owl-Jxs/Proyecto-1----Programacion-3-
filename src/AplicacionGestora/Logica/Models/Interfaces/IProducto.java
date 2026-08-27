cbd53ffcdd832ae6fc436754e1dc768574154bbb
Erving Sequeira <esequeira.dev@gmail.com>
2026-08-26 23:46:16 -0600

    feat: implementar modelo Producto con enum categoriaProducto

--- src/AplicacionGestora/Logica/Models/Interfaces/IProducto.java
+++ src/AplicacionGestora/Logica/Models/Interfaces/IProducto.java
@@ -12,12 +12,12 @@ public abstract class IProducto {
     public abstract void setId (int id);
     public abstract void setNombre (String nombre);
     public abstract void setPrecio (double precio);
-    public abstract void  setDireccionImg (String direccionImg);
+    public abstract void setCategoria(categoriaProducto categoria);
 
     public abstract int getId ();
     public abstract String getNombre ();
     public abstract double getPrecio ();
-    public abstract String getDireccionImg ();
+    public abstract categoriaProducto getCategoria();
     public abstract String toString ();
 
 }
