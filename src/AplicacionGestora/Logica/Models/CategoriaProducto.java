4d9c995fba754c39fc640c6b14fefc6b86a6fe5d
Erving Sequeira <esequeira.dev@gmail.com>
2026-09-02 16:33:40 -0600

    feat: Carpetas faltantes
    - Carpeta Comandos, Dao para implementacion y Vista para interfaz
    - README actualizado

--- src/AplicacionGestora/Logica/Models/CategoriaProducto.java
+++ src/AplicacionGestora/Logica/Models/CategoriaProducto.java
@@ -1,5 +1,6 @@
 package AplicacionGestora.Logica.Models;
 
+// Enum que representa las categorías de productos disponibles en el sistema de gestión.
 public enum CategoriaProducto {
     BEBIDA,
     DESAYUNO,
--- src/AplicacionGestora/Logica/Models/Producto.java
+++ src/AplicacionGestora/Logica/Models/Producto.java
@@ -1,6 +1,8 @@
 package AplicacionGestora.Logica.Models;
 import AplicacionGestora.Logica.Models.Interfaces.IProducto;
 
+// Clase que representa un producto en el sistema de gestión. 
+// Implementa la interfaz IProducto.
 public class Producto implements IProducto {
     private int id;
     private String nombre;
@@ -13,6 +15,7 @@ public class Producto implements IProducto {
         }
     }
 
+    // Constructor de la clase Producto, recibe nombre, precio y categoría del producto.
     public Producto(int id, String nombre, double precio, CategoriaProducto categoria) {
         if (id < 0) {
             throw new IllegalArgumentException("El id no puede ser negativo");
@@ -31,15 +34,22 @@ public class Producto implements IProducto {
         this.categoria = categoria;
     }
 
+    // Para settear el precio del producto.
     @Override public void setPrecio(double precio) {
         validarPrecio(precio);
         this.precio = precio;
     }
-
+    
+    // Obtener el id.
     @Override public int getId() { return id; }
+    // Obtener el nombre.
     @Override public String getNombre() { return nombre; }
+    // Obtener el precio.
     @Override public double getPrecio() { return precio; }
+    // Obtener la categoría del producto.
     @Override public CategoriaProducto getCategoria() { return categoria; }
+
+    // Metodo toString para representar el producto como una cadena de texto.
     @Override
     public String toString() {
         return "Producto Descripcion -> ID: " + id + " | Nombre: " + nombre + " | Precio: " + precio  + " | Categoria: " + categoria;
