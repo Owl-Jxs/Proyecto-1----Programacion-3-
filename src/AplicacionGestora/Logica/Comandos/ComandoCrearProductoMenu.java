99ec7e2122a32ce5803b546ffee9be28c4b6c681
seguraAriasAndres <andres.segura.arias@est.una.ac.cr>
2026-09-02 18:46:37 -0600

    Update ComandoCrearProductoMenu.java
    Añadicion de la nota de:
    
    Nota: NUNCA LLAMEN A EJECUTAR DIRECTAMENTE, Haganlo a travez del gestor. Ejemplo: gestorAcciones.ejecutarComando(comandoCrear);
    
    Porfavor Respeten Esto!

--- src/AplicacionGestora/Logica/Comandos/ComandoCrearProductoMenu.java
+++ src/AplicacionGestora/Logica/Comandos/ComandoCrearProductoMenu.java
@@ -43,6 +43,7 @@ public class ComandoCrearProductoMenu implements IComando {
         controladorCatalogo.agregarProducto(producto);
         ejecutado = true;
     }
+    //Nota, NUNCA LLAMEN A EJECUTAR DIRECTAMENTE, Haganlo a travez del gestor. Ejemplo: gestorAcciones.ejecutarComando(comandoCrear);
 
     @Override
     public void deshacer() {
