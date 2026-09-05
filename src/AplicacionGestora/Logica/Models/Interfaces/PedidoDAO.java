936af7e577c75ee9ffec881df03f3d791b49d82f
seguraAriasAndres <andres.segura.arias@est.una.ac.cr>
2026-09-04 19:45:51 -0600

    Update PedidoDAO.java
    Se modificó PedidoDAO para convertirlo en un contrato de acceso a datos, eliminando el almacenamiento y las implementaciones predeterminadas de la interfaz. Las operaciones para guardar, listar y buscar pedidos serán implementadas por una clase DAO concreta.

--- src/AplicacionGestora/Logica/Models/Interfaces/PedidoDAO.java
+++ src/AplicacionGestora/Logica/Models/Interfaces/PedidoDAO.java
@@ -1,29 +1,17 @@
 package AplicacionGestora.Logica.Models.Interfaces;
 
 import AplicacionGestora.Logica.Models.Pedido;
-import java.util.ArrayList;
+
+import java.util.List;
 
 public interface PedidoDAO {
 
-    ArrayList<Pedido> pedidos = new ArrayList<>();
-
     /*Guarda un pedido en el historial*/
-    default void guardarPedido(Pedido pedido) {
-        pedidos.add(pedido);
-    }
+    void guardarPedido(Pedido pedido);
 
     /*Obtiene todos los pedidos registrados en el historial*/
-    default ArrayList<Pedido> obtenerHistorialPedidos() {
-        return pedidos;
-    }
+    List<Pedido> obtenerHistorialPedidos();
 
     /*Busca un pedido utilizando su id*/
-    default Pedido obtenerPedidoPorId(int id) {
-        for (Pedido pedido : pedidos) {
-            if (pedido.getId() == id) {
-                return pedido;
-            }
-        }
-        return null;
-    }
+    Pedido obtenerPedidoPorId(int id);
 }
